5fb41da0a1b66e2abbb45d68fbbbea229f7307fa
FATEHUDDIN MUBASHIR <fatehuddin_mohammed@apple.com>
2016-10-16 16:02:43 -0700

    minor change

--- ContactBook/js/contactBook.js
+++ ContactBook/js/contactBook.js
@@ -1,16 +1,13 @@
 // controllers module, with ngResource module dependency to use $resource
-angular.module('ui.bootstrap.demo', ['ui.bootstrap','ngResource']);
-angular.module('ui.bootstrap.demo').controller('ContactFormCtrl', ['$scope','$resource', function ($scope,$resource) {
-	// initialize REST API access by creating Friend class
-	$scope.getContacts = $resource('https://candidate-test.herokuapp.com/contacts');
-		
-		//all static query() method of Friend class		
-  	   $scope.getContacts.query(function(data) {
-       $scope.contacts = data;
-       console.log('data', $scope.contacts)
-      });
+angular.module('ui.bootstrap.demo', ['ui.bootstrap', 'ngResource']);
+angular.module('ui.bootstrap.demo').controller('ContactFormCtrl', ['$scope', '$resource', function($scope, $resource) {
+    // initialize REST API access by creating Friend class
+    $scope.getContacts = $resource('https://candidate-test.herokuapp.com/contacts');
 
-  // Do something with myService
+    //all static query() method of Friend class		
+    $scope.getContacts.query(function(data) {
+        $scope.contacts = data;
+        //console.log('data', $scope.contacts)
+    });
+
 }]);
-
-	
